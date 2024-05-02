f3a1d56f8789fad5c63d061094c6da31680480eb
isabeladomingoss <domingosisabela@icloud.com>
2024-05-02 01:06:57 -0300

    atualizacoes

---
+++ tipos_de_dados.py
@@ -0,0 +1,12 @@
+print(1 + 10 + 162782)
+
+print(1.5 + 3 + 5.8)
+
+
+print(True)
+
+print (False)
+
+print("Python")
+
+int
---
+++ variaveis_constantes.py
@@ -0,0 +1,10 @@
+nome = 'Guilherme'
+idade = 28
+
+nome = 'Giovanna'
+
+print(nome, idade)
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
